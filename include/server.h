fa2e2e8f1cc1f51d08c2ec61f7f97ea2d482a43e
montybytes <muturimonty@gmail.com>
2024-12-28 20:00:04 +0000

    implemented config text file loading

--- include/server.h
+++ include/server.h
@@ -1,6 +1,12 @@
 #ifndef SERVER_H
 #define SERVER_H
 
+#include <unordered_map>
+#include <string>
+#include <variant>
+
+#include "file.h"
+
 using namespace std;
 
 class Server
@@ -12,6 +18,10 @@ public:
 
 private:
     int serverSocket;
+    unordered_map<string, string> configs;
+
+    void loadConfig();
+    File getStaticFile(const string &path);
 };
 
 #endif
--- src/server.cpp
+++ src/server.cpp
@@ -8,14 +8,66 @@
 #include "response.h"
 #include "file.h"
 #include "error.h"
+#include "string_utils.h"
+
+// Load server's configurations from file
+void Server::loadConfig()
+{
+    ifstream configFile("config.ini");
+
+    if (!configFile.is_open())
+    {
+        cerr << "Error opening the configuration file!";
+        throw;
+    }
+
+    string configLine;
+    string configSectionName = "";
+
+    while (getline(configFile, configLine))
+    {
+        configLine = trim(configLine);
+
+        if (configLine == "")
+        {
+            continue;
+        }
+
+        size_t sectionStart = configLine.find_first_of("[");
+        size_t sectionEnd = configLine.find_last_of("]");
+
+        if (sectionStart != string::npos &&
+            sectionEnd != string::npos &&
+            sectionStart != sectionEnd)
+        {
+            configSectionName = configLine.substr(sectionStart + 1, sectionEnd - sectionStart - 1);
+        }
+
+        bool isValidConfig = configLine.find_first_of("=") != string::npos;
+
+        if (sectionStart == string::npos && isValidConfig)
+        {
+            vector<string> config = splitString(configLine, "=");
+
+            string key = trim(configSectionName) + "_" + trim(config[0]);
+            string value = trim(config[1]);
+
+            this->configs[key] = value;
+        }
+    }
+
+    configFile.close();
+}
 
 // Initialize server & listen to incoming connections
 void Server::initialize(const int &port)
 {
+    this->loadConfig();
+
     sockaddr_in address;
 
     address.sin_family = AF_INET;
-    address.sin_port = htons(port);
+    address.sin_port = htons(stoi(this->configs.at("server_port")));
     address.sin_addr.s_addr = INADDR_ANY;
 
     serverSocket = socket(AF_INET, SOCK_STREAM, 0);
@@ -41,11 +93,8 @@ void Server::start()
 
             const Request clientRequest = Request::parseRequest(requestBuffer);
 
-            // map url to file system
-            const string requestedFilePath = File::mapPathToAbsolute(clientRequest.resource.target.path);
-
             // read file from directory
-            const File requestedFile = File::fromPath(requestedFilePath);
+            const File requestedFile = this->getStaticFile(clientRequest.resource.target.path.substr(1));
 
             Response response = Response("HTTP/1.1 200 OK");
 
@@ -76,4 +125,34 @@ void Server::start()
 void Server::stop()
 {
     close(serverSocket);
+}
+
+File Server::getStaticFile(const string &path)
+{
+    const fs::path root = this->configs["server_document_root"];
+
+    string defaultFileName = "index.html";
+    string absolutePath;
+
+    // if the root file is requested
+    if (path == "" || path == "/")
+    {
+        // if the root file name has been defined in the config file and is not empty set to definition
+        if (this->configs.count("server_root_file") && this->configs["server_root_file"] != "")
+        {
+            absolutePath = File::mapPathToAbsolute(root / this->configs["server_root_file"]);
+        }
+        else
+        {
+            absolutePath = File::mapPathToAbsolute(root / "index.html");
+        }
+    }
+    else
+    {
+        absolutePath = File::mapPathToAbsolute(root / path);
+    }
+
+    cout << absolutePath << endl;
+
+    return File::fromPath(absolutePath);
 }
